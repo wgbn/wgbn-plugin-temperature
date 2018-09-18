f03e93dea7537473d34ad4bb2714381a0ab6a260
Walter Gandarella <walter@vivasalute.com.br>
2018-09-18 15:53:45 -0300

    package-readme

--- www/Temperature.js
+++ www/Temperature.js
@@ -1,7 +1,7 @@
 var exec = require('cordova/exec');
 
-exports.checkTemperature = function (success, error) {
-    exec(success, error, 'TemperaturePlugin', 'checkTemperature');
+exports.checkTemperature = function(success, error) {
+    exec(success, error, 'Temperature', 'checkTemperature');
 };
 
 exports.isDeviceCompatible = function(success, error) {
